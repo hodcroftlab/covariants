d0e120bcf77246c536fd2d1b88f686ccf4668877
Richard Neher <richard.neher@unibas.ch>
2020-11-14 20:47:30 +0100

    add belgium, combine travel panels

--- travel_volume_figure.py
+++ travel_volume_figure.py
@@ -7,10 +7,11 @@ from helpers import *
 figure_path = '../cluster_scripts/figures/'
 
 
-def get_import_frequency(country, cases, weeks=None, avg_cases_per_intro=1):
+def get_import_frequency(country, cases, frequency, weeks=None, avg_cases_per_intro=1):
     if weeks is None:
         weeks = list(range(15,44))
 
+    summer_average = np.mean([x for d,x in frequency.items() if d.month>7])
     imported_incidence = []
     import_totals = [0]
     totals = [0]
@@ -26,7 +27,7 @@ def get_import_frequency(country, cases, weeks=None, avg_cases_per_intro=1):
         # numbers are per month. Hence divide by 30 and multiply by 7 to obtain rates per week\.
         travel_rate = travel_volume[country][mid_month]/popsizes[country]/30*7
 
-        imported_incidence.append(avg_cases_per_intro*cases["Spain"][d]/popsizes["Spain"]*travel_rate)
+        imported_incidence.append(avg_cases_per_intro*cases["Spain"][d]/popsizes["Spain"]*travel_rate*frequency.get(d, 0 if d.month<7 else summer_average))
 
         # rate of change not due to imports
         Re = (cases[country][d]/popsizes[country] - imported_incidence[-1])/(cases[country][prev_week]/popsizes[country])
@@ -43,35 +44,39 @@ def get_import_frequency(country, cases, weeks=None, avg_cases_per_intro=1):
     return {"frequency":import_totals/totals, "dates":dates, "Re": Re_traj, "introductions":introductions}
 
 
+spain_frequency = {k: c/tot for k, c, tot in zip(*non_zero_counts(cluster_data, total_data, "Spain"))}
+
+
 fs=14
 fmt = 'pdf'
+countries = ["Switzerland", "Spain", "United Kingdom", "Netherlands", "France", "Ireland", "Denmark", "Scotland", "Wales", "Belgium"]
 
-fig = plt.figure()
-for coun in travel_volume:
-   plt.plot(travel_volume[coun].index, travel_volume[coun]/popsizes[coun]*100000, label=coun,
-            c=country_styles[coun]['c'], lw=2, ls=country_styles[coun].get('ls', '-'))
+fig, axs = plt.subplots(2,1, figsize=(6,7), sharex=True)
+for country in countries:
+    if country=='Spain':
+        continue
+    axs[0].plot(travel_volume[country].index, travel_volume[country]/popsizes[country]*100000, label=country,
+            c=country_styles[country]['c'], lw=2, ls=country_styles[country].get('ls', '-'))
 
-plt.yscale('log')
-plt.ylabel('passengers/100000', fontsize=fs)
+axs[0].set_yscale('log')
+axs[0].set_ylabel('dept. from Spain/100k residents', fontsize=fs)
+axs[1].tick_params(labelsize=fs*0.8)
 fig.autofmt_xdate(rotation=30)
-plt.legend()
-plt.savefig(figure_path+f'travel_volume.{fmt}')
+# plt.savefig(figure_path+f'travel_volume.{fmt}')
 
-
-countries = ["Switzerland", "Spain", "United Kingdom", "Netherlands", "France", "Ireland", "Denmark", "Scotland", "Wales"]
 case_data = load_case_data(countries)
-fig = plt.figure()
+# fig = plt.figure()
 
 for country in case_data:
     if country=='Spain':
         continue
+    res = get_import_frequency(country, case_data, spain_frequency)
+    print(f"{country} -- total imports: {np.sum(res['introductions'])}")
+    axs[1].plot(res['dates'], res['frequency'], label=country, c=country_styles[country]['c'], ls=country_styles[country].get('ls', '-'))
 
-    res = get_import_frequency(country, case_data)
-    plt.plot(res['dates'], res['frequency'], label=country, c=country_styles[country]['c'], ls=country_styles[country].get('ls', '-'))
-
-plt.legend(fontsize=fs*0.8)
-plt.tick_params(labelsize=fs*0.8)
-plt.ylabel('Cumulative frequency of imports', fontsize=fs)
+axs[1].legend(fontsize=fs*0.9)
+axs[1].tick_params(labelsize=fs*0.8)
+axs[1].set_ylabel('naive frequency of imports', fontsize=fs)
 fig.autofmt_xdate(rotation=30)
 plt.tight_layout()
 plt.savefig(figure_path+f'import_model.{fmt}')
@@ -95,7 +100,7 @@ for country in countries:
     if country=="Spain":
         continue
 
-    res = get_import_frequency(country, case_data)
+    res = get_import_frequency(country, case_data,spain_frequency)
     scale_factor = np.mean([cluster_freq[i] for i,d in enumerate(week_as_date) if d in res['dates']])/np.mean(res["frequency"])
     plt.plot(res["dates"], res["frequency"]*scale_factor,
             c=country_styles[country]['c'], ls=country_styles[country]['ls'],
