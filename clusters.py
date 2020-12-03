ff803a3269a2ca54594d65adc51d056854d88d64
Emma Hodcroft <emmahodcroft@gmail.com>
2020-12-03 17:30:51 +0100

    add Denmark and 626

--- clusters.py
+++ clusters.py
@@ -15,7 +15,13 @@ clusters = {"S222": {'snps': [22226, 28931, 29644], 'cluster_data': [],
             "country_info":[], 'col': "#cc0000", "display_name": "S:Y453F", "build_name":"S.Y453F"},
             
             "S501": {'snps': [23063], 'cluster_data': [], 'snps2': [23062],
-            "country_info":[], 'col': "#ff99ff", "display_name": "S:N501", "build_name":"S.N501"}
+            "country_info":[], 'col': "#ff99ff", "display_name": "S:N501", "build_name":"S.N501"},
+
+            "S626": {'snps': [23437, 771], 'cluster_data': [],
+            "country_info":[], 'col': "#ffffff", "display_name": "S:A626S", "build_name":"S.A626S"},
+
+            "DanishCluster": {'snps': [15655], 'cluster_data': [],
+            "country_info":[], 'col': "#ffffff", "display_name": "DanishCluster", "build_name":"DanishCluster"}
 
             #"S69": {'snps': [21766], 'cluster_data': [], 'build_name': "S.69"},
             #"S692": {'snps': [23635], 'cluster_data': [], 'build_name': "S.692"},
