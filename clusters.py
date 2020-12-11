649684379cfbf8a3bda80cbc39bfa64e831337a5
Emma Hodcroft <emmahodcroft@gmail.com>
2020-12-11 15:49:21 +0100

    add tempcluster

--- clusters.py
+++ clusters.py
@@ -26,6 +26,7 @@ clusters = {"S222": {'snps': [22226, 28931, 29644], 'cluster_data': [],
             #"S69": {'snps': [21766], 'cluster_data': [], 'build_name': "S.69"},
             #"S692": {'snps': [23635], 'cluster_data': [], 'build_name': "S.692"},
             #"S1229": {'snps': [25248], 'cluster_data': [], 'build_name': "S.1229"}
+            #"S1122": {'snps': [24925,9119], 'build_name': 'S.1122'} #mostly Swedish cluster
             }
 
 
