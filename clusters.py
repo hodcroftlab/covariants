73a29bb6d0001d220353ee2d4bb8f3cb6c76c8d2
Emma Hodcroft <emmahodcroft@gmail.com>
2020-12-19 15:46:30 +0100

    temp cluster

--- clusters.py
+++ clusters.py
@@ -33,5 +33,6 @@ clusters = {"S222": {'snps': [22226, 28931, 29644], 'cluster_data': [],
             #"S692": {'snps': [23635], 'cluster_data': [], 'build_name': "S.692"},
             #"S1229": {'snps': [25248], 'cluster_data': [], 'build_name': "S.1229"}
             #"S1122": {'snps': [24925,9119], 'build_name': 'S.1122'} #mostly Swedish cluster
+            #"S655": {'snps': [23524], 'cluster_data': [], "display_name": "S:H655Y", 'build_name': "S.H655Y"}
             }
 
