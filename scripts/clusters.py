50c4c2ca9ee4132eb1bb7f64e3757f4e753e5696
Emma Hodcroft <emmahodcroft@gmail.com>
2021-01-03 16:13:03 +0100

    change order of clusters

--- scripts/clusters.py
+++ scripts/clusters.py
@@ -2,32 +2,37 @@
 
 clusters = {"S222": {'snps': [22226, 28931, 29644], 'cluster_data': [],
             "country_info":[], 'col': "#ff8d3d", "display_name": "20A.EU1", "build_name":"20A.EU1"},
+
             "S477": {'snps': [22991, 4542], 'cluster_data': [],
             'country_info':[], 'col': "#65beeb", "display_name": "20A.EU2", "build_name":"20A.EU2"},
-            "S98": {'snps': [21854, 25504], 'cluster_data': [],
-            "country_info":[], 'col': "#911eb4", "display_name": "S:S98F", "build_name":"S.S98F"},
-            "S80": {'snps': [21799, 3098], 'cluster_data': [],
-            "country_info":[], 'col': "#3cb44b", "display_name": "S:D80Y", "build_name":"S.D80Y"},
+
+            "S501": {'snps': [23063], 'cluster_data': [], 'snps2': [23062],
+            "country_info":[], 'col': "#ff99ff", "display_name": "S:N501", "build_name":"S.N501"},
+
+            "S69": {'snps': [], 'cluster_data': [], 'gaps': [21766,21767,21768],
+            "country_info":[], 'col': "#ffcc00", "display_name": "S:H69-", "build_name":"S.H69-"},
+
             "S439": {'snps': [7766, 22878], 'cluster_data': [],
             "country_info":[], 'col': "#fb9a99", "display_name": "S:N439K", "build_name":"S.N439K"},
 
             "S453": {'snps': [22919], 'cluster_data': [],
             "country_info":[], 'col': "#cc0000", "display_name": "S:Y453F", "build_name":"S.Y453F"},
             
-            "S501": {'snps': [23063], 'cluster_data': [], 'snps2': [23062],
-            "country_info":[], 'col': "#ff99ff", "display_name": "S:N501", "build_name":"S.N501"},
+            "S98": {'snps': [21854, 25504], 'cluster_data': [],
+            "country_info":[], 'col': "#911eb4", "display_name": "S:S98F", "build_name":"S.S98F"},
+
+            "S80": {'snps': [21799, 3098], 'cluster_data': [],
+            "country_info":[], 'col': "#3cb44b", "display_name": "S:D80Y", "build_name":"S.D80Y"},
 
             "S626": {'snps': [23437, 771], 'cluster_data': [],
             "country_info":[], 'col': "#0000ff", "display_name": "S:A626S", "build_name":"S.A626S"},
 
-            "DanishCluster": {'snps': [15655], 'cluster_data': [],
-            "country_info":[], 'col': "#ffffff", "display_name": "DanishCluster", "build_name":"DanishCluster"},
-
-            "S69": {'snps': [], 'cluster_data': [], 'gaps': [21766,21767,21768],
-            "country_info":[], 'col': "#ffcc00", "display_name": "S:H69-", "build_name":"S.H69-"},
-
             "S1122": {'snps': [24925,9119],  'cluster_data': [], #mostly Swedish cluster
-            "country_info":[], 'col': "#660066", "display_name": "S:V1122L", 'build_name': 'S.V1122L'}
+            "country_info":[], 'col': "#660066", "display_name": "S:V1122L", 'build_name': 'S.V1122L'},
+
+            "DanishCluster": {'snps': [15655], 'cluster_data': [],
+            "country_info":[], 'col': "#ffffff", "display_name": "DanishCluster", "build_name":"DanishCluster"}
+
 
             #"S69": {'snps': [21766], 'cluster_data': [], 'build_name': "S.69"},
             #"S692": {'snps': [23635], 'cluster_data': [], 'build_name': "S.692"},
