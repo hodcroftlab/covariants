db1fa74e2184b2af48fe8818ba757abe3f250772
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-01-18 17:30:20 +0100

    fix(web): correct nav link highlighting

--- web/src/components/Layout/NavigationBar.tsx
+++ web/src/components/Layout/NavigationBar.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
+import { FaGithub, FaTwitter } from 'react-icons/fa'
 
 import { connect } from 'react-redux'
-import { FaGithub, FaTwitter } from 'react-icons/fa'
+
+import { ReactComponent as BrandLogo } from 'src/assets/images/logo.svg'
+import { NavigationLink } from 'src/components/Layout/NavigationLink'
+
+import { Link } from 'src/components/Link/Link'
+import { LinkExternal } from 'src/components/Link/LinkExternal'
 import { TWITTER_USERNAME_RAW, URL_GITHUB } from 'src/constants'
 
 import { State } from 'src/state/reducer'
 import { selectPathname } from 'src/state/router/router.selectors'
-
-import { Link } from 'src/components/Link/Link'
-import { LinkExternal } from 'src/components/Link/LinkExternal'
-import { NavigationLink } from 'src/components/Layout/NavigationLink'
-
-import { ReactComponent as BrandLogo } from 'src/assets/images/logo.svg'
 
 const navLinksLeft = {
   '/': 'Home',
@@ -21,18 +21,26 @@ const navLinksLeft = {
   '/per-variant': 'Per variant',
 }
 
+export function matchingUrl(url: string, pathname: string): boolean {
+  if (pathname.startsWith('/variants')) {
+    return url === '/variants'
+  }
+
+  return url === pathname
+}
+
 const navLinksRight = [
   {
     title: 'Follow me on Twitter',
     url: `https://twitter.com/${TWITTER_USERNAME_RAW}`,
     alt: 'Link to Twitter, with blue Twitter bird logo',
-    icon: <FaTwitter size={28} color="#08a0e9" />,
+    icon: <FaTwitter size={28} color='#08a0e9' />,
   },
   {
-    title: "Let's collaborate on GitHub",
+    title: 'Let\'s collaborate on GitHub',
     url: URL_GITHUB,
     alt: 'Link to Github page, with grey Github Octocat logo',
-    icon: <FaGithub size={28} color="#24292E" />,
+    icon: <FaGithub size={28} color='#24292E' />,
   },
 ]
 
@@ -51,27 +59,27 @@ export const NavigationBar = connect(mapStateToProps, mapDispatchToProps)(Naviga
 export function NavigationBarDisconnected({ pathname }: NavigationBarProps) {
   return (
     <nav
-      className="navbar navbar-expand navbar-light navbar-scroll hide-native-scrollbar"
-      role="navigation"
-      data-testid="NavigationBar"
+      className='navbar navbar-expand navbar-light navbar-scroll hide-native-scrollbar'
+      role='navigation'
+      data-testid='NavigationBar'
     >
-      <Link className="navbar-brand d-flex" href="/" role="button">
-        <BrandLogo className="navigation-bar-product-logo" />
+      <Link className='navbar-brand d-flex' href='/' role='button'>
+        <BrandLogo className='navigation-bar-product-logo' />
       </Link>
 
-      <ul className="navbar-nav">
+      <ul className='navbar-nav'>
         {Object.entries(navLinksLeft).map(([url, text]) => {
           return (
-            <NavigationLink key={url} url={url} active={pathname.startsWith(url)}>
+            <NavigationLink key={url} url={url} active={matchingUrl(url, pathname)}>
               {text}
             </NavigationLink>
           )
         })}
       </ul>
 
-      <ul className="navbar-nav ml-auto d-flex">
+      <ul className='navbar-nav ml-auto d-flex'>
         {navLinksRight.map(({ title, url, alt, icon }) => (
-          <li key={title} className="nav-item mx-2 my-auto">
+          <li key={title} className='nav-item mx-2 my-auto'>
             <LinkExternal title={title} href={url} alt={alt} icon={null}>
               {icon}
             </LinkExternal>
