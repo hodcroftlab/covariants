46622b50f2020a948251ee095e1c74b4a82c4abc
Emma Hodcroft <emmahodcroft@gmail.com>
2021-01-25 16:11:20 +0100

    add UK countries plot

--- clusterDynamics.py
+++ clusterDynamics.py
@@ -476,6 +476,8 @@ for clus in clus_to_run:
             #for a simpler plot of most interesting countries use this:
             for coun in [ 'England', 'Scotland', 'Wales', 'Northern Ireland', 'United Kingdom']:
                 week_as_date, cluster_count, total_count = non_zero_counts(cluster_data, total_data, coun, smoothing=smoothing)
+                # remove last data point if that point as less than frac sequences compared to the previous count
+                week_as_date, cluster_count, total_count = trim_last_data_point(week_as_date, cluster_count, total_count, frac=0.2, keep_count=10)
 
                 if coun == 'United Kingdom':
                     print('UK')
