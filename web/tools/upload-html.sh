fbd9451643c57101f15b334075b7d17407e89dc5
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-01-29 13:30:12 +0100

    chore: fix html deployment script

--- web/tools/upload-html.sh
+++ web/tools/upload-html.sh
@@ -13,10 +13,10 @@ if [ "${TRAVIS_BRANCH}" = "master" ]; then
   S3_BUCKET="${S3_BUCKET}-master"
 fi
 
-find * -type f -name "*.html" -exec bash -c '\
-aws s3 cp --content-type "text/html" --metadata-directive REPLACE \
-s3://${S3_BUCKET}/$1 \
-s3://${S3_BUCKET}/${1%.html}' \
-- "{}" \;
+find * -type f -name "*.html" -exec bash -c "\
+aws s3 cp --content-type 'text/html' --metadata-directive REPLACE \
+s3://${S3_BUCKET}/\$1 \
+s3://${S3_BUCKET}/\${1%.html}" \
+- '{}' \;
 
 popd >/dev/null
