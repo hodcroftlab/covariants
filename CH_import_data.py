1890e2d489a5e031d2532bf9f59c96df37206213
Richard Neher <richard.neher@unibas.ch>
2021-02-09 14:17:19 +0100

    add swiss import data

--- CH_import_data.py
+++ CH_import_data.py
@@ -5,14 +5,14 @@ from travel_volume_figure import *
 from collections import defaultdict
 
 
-a = pd.read_csv('travel_data/switzerland_imports_from_spain.csv')
+a = pd.read_csv('travel_data/2021-02-03_SwissImportsFromSpain_JunSep.csv')
 a["Date"] = pd.to_datetime(a.Date)
 
 imports_by_CW = {cw:0 for cw in range(15,50)}
 
 for ri, row in a.iterrows():
     cw = date_to_CW(row.Date)
-    imports_by_CW[cw] += row.Spain_cases
+    imports_by_CW[cw] += row.Cases_Imports_Spain
 
 b = pd.DataFrame(imports_by_CW.values(), index=imports_by_CW.keys())
 
