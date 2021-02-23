8846b680bbb6a10d95a2c584032afa23a01ef756
Emma Hodcroft <emmahodcroft@gmail.com>
2021-02-23 20:22:21 +0100

    adjust line styles & colors

--- web/src/helpers/lineStyleToStrokeDashArray.ts
+++ web/src/helpers/lineStyleToStrokeDashArray.ts
@@ -1,9 +1,9 @@
 import { get } from 'lodash'
 
 export const lineStyles: Record<string, string | undefined> = {
-  ':': '1 2',
-  '-.': '3 3 1 3',
-  '--': '3 2',
+  ':': '3 3',
+  '-.': '5 3 2 3 2 3',
+  '--': '8 6',
   '-': undefined,
 }
 
