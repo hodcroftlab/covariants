c152e7480d1d733e9878503b0933e427f6fc77cc
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-02-24 16:27:48 +0100

    refactor: lint

--- web/src/io/getClusterEpiIslsNumChunks.ts
+++ web/src/io/getClusterEpiIslsNumChunks.ts
@@ -4,21 +4,14 @@ import acknowledgements from 'src/../data/acknowledgements/acknowledgements_keys
 
 export function getClusterEpiIslsNumChunks(clusterBuildName: string): number {
   const acks = get(acknowledgements, 'acknowledgements')
-
   if (!acks) {
     console.warn(`Acknowledgements not found`)
   }
 
-  const numChunksStr = get(acks, clusterBuildName)
-
-  if (!numChunksStr) {
+  const ack = get<Record<string, { numChunks: number } | undefined>, string>(acks, clusterBuildName)
+  if (!ack) {
     throw new Error(`Acknowledgements not found for ${clusterBuildName}`)
   }
 
-  const numChunks = Number.parseInt(numChunksStr.numChunks, 10)
-  if (!numChunks) {
-    throw new Error(`Acknowledgements are corrupted for ${clusterBuildName}`)
-  }
-
-  return numChunks
+  return ack.numChunks
 }
