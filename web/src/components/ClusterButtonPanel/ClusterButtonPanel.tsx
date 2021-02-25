d506fdca5c599ffe82f8edb60874c0862bcb8e29
Emma Hodcroft <emmahodcroft@gmail.com>
2021-02-25 22:48:43 +0100

    add dividers for buttons

--- web/src/components/ClusterButtonPanel/ClusterButtonPanel.tsx
+++ web/src/components/ClusterButtonPanel/ClusterButtonPanel.tsx
@@ -10,6 +10,13 @@ const ClustersRow = styled(Row)`
   justify-content: center;
 `
 
+const ClustersSubheading = styled.div`
+  text-align: left;
+  width: 100%;
+  padding-left: 5px;
+  color: var(--gray);
+`
+
 export interface ClusterPanelProps {
   clusters: ClusterDatum[]
   currentCluster?: ClusterDatum
@@ -17,15 +24,32 @@ export interface ClusterPanelProps {
 }
 
 export function ClusterButtonPanel({ clusters, currentCluster, className }: ClusterPanelProps) {
+  const groupedClusters = groupClusters(clusters)
   return (
     <ClustersRow noGutters className={className}>
-      {clusters.map((cluster) => (
-        <ClusterButton
-          key={cluster.display_name}
-          cluster={cluster}
-          isCurrent={cluster.display_name === currentCluster?.display_name}
-        />
+      {Object.keys(groupedClusters).map((clusterType) => (
+        <>
+          <ClustersSubheading>{clusterType}</ClustersSubheading>
+          {groupedClusters[clusterType].map((cluster) => (
+            <ClusterButton
+              key={cluster.display_name}
+              cluster={cluster}
+              isCurrent={cluster.display_name === currentCluster?.display_name}
+            />
+          ))}
+        </>
       ))}
     </ClustersRow>
   )
+}
+
+function groupClusters(clusters: ClusterDatum[]) {
+  type tGroupedClusters = { [key: string]: Array<ClusterDatum> }
+  const groupedClusters: tGroupedClusters = {}
+  clusters.forEach((cluster) => {
+    const clusterType = cluster.type || 'other'
+    if (groupedClusters[clusterType] === undefined) groupedClusters[clusterType] = []
+    groupedClusters[clusterType].push(cluster)
+  })
+  return groupedClusters
 }
