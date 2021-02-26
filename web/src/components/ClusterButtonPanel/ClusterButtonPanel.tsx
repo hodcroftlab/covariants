57c7781682c46a408527a34ab18321d9559a8299
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-02-26 14:30:10 +0100

    refactor: group clusters only once, instead of on every render

--- web/src/components/ClusterButtonPanel/ClusterButtonPanel.tsx
+++ web/src/components/ClusterButtonPanel/ClusterButtonPanel.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import { Row } from 'reactstrap'
-import { ClusterDatum } from 'src/io/getClusters'
+import { ClusterDatum, getClustersGrouped } from 'src/io/getClusters'
 import styled from 'styled-components'
 
 import { ClusterButton } from './ClusterButton'
+
+const clustersGrouped = getClustersGrouped()
 
 const ClustersRow = styled(Row)`
   justify-content: center;
@@ -18,38 +20,25 @@ const ClustersSubheading = styled.div`
 `
 
 export interface ClusterPanelProps {
-  clusters: ClusterDatum[]
   currentCluster?: ClusterDatum
   className?: string
 }
 
-export function ClusterButtonPanel({ clusters, currentCluster, className }: ClusterPanelProps) {
-  const groupedClusters = groupClusters(clusters)
+export function ClusterButtonPanel({ currentCluster, className }: ClusterPanelProps) {
   return (
     <ClustersRow noGutters className={className}>
-      {Object.keys(groupedClusters).map((clusterType) => (
-        <>
+      {Object.entries(clustersGrouped).map(([clusterType, clusterGroup]) => (
+        <Fragment key={clusterType}>
           <ClustersSubheading>{clusterType}</ClustersSubheading>
-          {groupedClusters[clusterType].map((cluster) => (
+          {clusterGroup.map((cluster) => (
             <ClusterButton
               key={cluster.display_name}
               cluster={cluster}
               isCurrent={cluster.display_name === currentCluster?.display_name}
             />
           ))}
-        </>
+        </Fragment>
       ))}
     </ClustersRow>
   )
-}
-
-function groupClusters(clusters: ClusterDatum[]) {
-  type tGroupedClusters = { [key: string]: Array<ClusterDatum> }
-  const groupedClusters: tGroupedClusters = {}
-  clusters.forEach((cluster) => {
-    const clusterType = cluster.type || 'other'
-    if (groupedClusters[clusterType] === undefined) groupedClusters[clusterType] = []
-    groupedClusters[clusterType].push(cluster)
-  })
-  return groupedClusters
 }
--- web/src/components/Home/HomePage.tsx
+++ web/src/components/Home/HomePage.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 
 import { Col, Row } from 'reactstrap'
 
-import { getClusters } from 'src/io/getClusters'
-
 import { ClusterButtonPanel } from 'src/components/ClusterButtonPanel/ClusterButtonPanel'
 import { VariantsPageContainer } from 'src/components/Common/ClusterSidebarLayout'
 import { Editable } from 'src/components/Common/Editable'
 import { Layout } from 'src/components/Layout/Layout'
 
 import HomeContent from '../../../../content/Home.md'
-
-const clusters = getClusters()
 
 export function HomePage() {
   return (
@@ -25,7 +21,7 @@ export function HomePage() {
 
         <Row noGutters>
           <Col lg={3} xl={2}>
-            <ClusterButtonPanel clusters={clusters} />
+            <ClusterButtonPanel />
           </Col>
 
           <Col lg={9} xl={10}>
