64d8b041ea2825fe347b61ce7b2394cd739d15ad
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-03-11 15:35:45 +0100

    fix: ensure variant badges work for named variants

---
+++ web/src/components/Common/__tests__/parseVariant.test.ts
@@ -0,0 +1,51 @@
+import { parseVariant } from 'src/components/Common/parseVariant'
+
+describe('parseVariant', () => {
+  it('should accept "20A.EU2"', async () => {
+    expect(parseVariant('20A.EU2')).toEqual({
+      parent: '20A',
+      parentDelimiter: undefined,
+      gene: undefined,
+      left: undefined,
+      pos: undefined,
+      right: undefined,
+      version: '.EU2',
+    })
+  })
+
+  it('should accept "20E (EU1)"', async () => {
+    expect(parseVariant('20E (EU1)')).toEqual({
+      parent: '20E',
+      parentDelimiter: undefined,
+      gene: undefined,
+      left: undefined,
+      pos: undefined,
+      right: undefined,
+      version: ' (EU1)',
+    })
+  })
+
+  it('should accept "20I/501Y.V1"', async () => {
+    expect(parseVariant('20I/501Y.V1')).toEqual({
+      parent: '20I',
+      parentDelimiter: '/',
+      gene: undefined,
+      left: undefined,
+      pos: 501,
+      right: 'Y',
+      version: '.V1',
+    })
+  })
+
+  it('should accept "20C/S:452R"', async () => {
+    expect(parseVariant('20C/S:452R')).toEqual({
+      parent: '20C',
+      parentDelimiter: '/',
+      gene: 'S',
+      left: undefined,
+      pos: 452,
+      right: 'R',
+      version: undefined,
+    })
+  })
+})
