745b5136cd7fd6fa2b7b4e85809ca47a69c0c0ac
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-04-29 05:53:04 +0200

    adjust number of x-axis ticks responsively, improve date formatting

--- web/src/helpers/format.ts
+++ web/src/helpers/format.ts
@@ -6,6 +6,8 @@ export const formatInteger = (value: number) => value.toFixed(0)
 
 export const formatDate = (date: number) => DateTime.fromSeconds(date).toISODate()
 
+export const formatDateHumanely = (date: number) => DateTime.fromSeconds(date).toFormat('MMM yyyy').replace(' ', '\n')
+
 export function dateStringToSeconds(date: string): number {
   return DateTime.fromFormat(date, 'yyyy-MM-dd').toSeconds()
 }
