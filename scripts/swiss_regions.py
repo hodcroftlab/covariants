439865ecb8f9ab5d6b1f933491c0ad77ee21441e
Emma Hodcroft <emmahodcroft@gmail.com>
2021-05-26 01:17:44 +0200

    add swiss regions

---
+++ scripts/swiss_regions.py
@@ -0,0 +1,37 @@
+swiss_regions = {
+    'Geneva': "Region 1",
+    "Neuchâtel": "Region 1",
+    "Vaud": "Region 1",
+    "Valais": "Region 1",
+    "Bern": "Region 2",
+    "Fribourg": "Region 2",
+    "Jura": "Region 2",
+    "Aargau": "Region 3",
+    "Basel-Land": "Region 3",
+    "Basel-Stadt": "Region 3",
+    "Solothurn": "Region 3",
+    "Lucerne": "Region 4",
+    "Nidwalden": "Region 4",
+    "Obwalden": "Region 4",
+    "Schwyz": "Region 4",
+    "Uri": "Region 4",
+    "Zug": "Region 4",
+    "Appenzell Innerrhoden": "Region 5",
+    "Appenzell Ausserrhoden": "Region 5",
+    "Glarus": "Region 5",
+    "Sankt Gallen": "Region 5",
+    "Schaffhausen": "Region 5",
+    "Thurgau": "Region 5",
+    "Zürich": "Region 5",
+    "Graubünden": "Region 6",
+    "Ticino": "Region 6",
+    "Switzerland": ""
+}
+
+
+# region 1 «GE, NE, VD, VS»
+# region 2 «BE, FR, JU»
+# region 3 «AG, BL, BS, SO»
+# region 4 «LU, NW, OW, SZ, UR, ZG»
+# region 5 «AI, AR, GL, SG, SH, TG, ZH»
+# region 6 «GR, TI».
