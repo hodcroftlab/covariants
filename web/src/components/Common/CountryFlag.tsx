f877c0f84cef939fdd8fb4b4ef0418929d1007e7
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-06-28 06:55:46 +0200

    feat: show grey rectangle when no flag found

--- web/src/components/Common/CountryFlag.tsx
+++ web/src/components/Common/CountryFlag.tsx
@@ -1,13 +1,20 @@
 import React, { SVGProps } from 'react'
 
+import styled from 'styled-components'
 import iso3311a2 from 'iso-3166-1-alpha-2'
 import Flags from 'country-flag-icons/react/3x2'
 
-const missingCountryCodes: Record<string, string> = {
+export const missingCountryCodes: Record<string, string> = {
   'USA': 'US',
   'Russia': 'RU',
   'South Korea': 'KR',
 }
+
+export const FlagDefault = styled.div<{ $countryCode?: string }>`
+  height: 1em;
+  width: 1.5em;
+  background-color: #aaa;
+`
 
 export interface CountryFlagProps extends SVGProps<SVGSVGElement> {
   country: string
@@ -16,5 +23,5 @@ export interface CountryFlagProps extends SVGProps<SVGSVGElement> {
 export function CountryFlag({ country, style }: CountryFlagProps) {
   const countryCode = missingCountryCodes[country] ?? iso3311a2.getCode(country) ?? '?'
   const Flag = Flags[countryCode]
-  return Flag ? <Flag style={{ height: '1em', ...style }} /> : null
+  return Flag ? <Flag style={{ height: '1em', ...style }} /> : <FlagDefault $countryCode={countryCode} />
 }
