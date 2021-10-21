a2abe4f70cbcc1d1d016830b7097e0f6d226824d
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-10-21 14:52:40 +0200

    feat: subset mutation counts table, display badges

--- web/src/components/MutationCounts/MutationCountsSummary.tsx
+++ web/src/components/MutationCounts/MutationCountsSummary.tsx
@@ -1,11 +1,29 @@
 import React, { useMemo } from 'react'
 
+import { partition } from 'lodash'
 import { useQuery } from 'react-query'
-
-import { TableSlim } from 'src/components/Common/TableSlim'
+import { Row, Col } from 'reactstrap'
 
 import type { ClusterDatum } from 'src/io/getClusters'
 import { getMutationCounts, MutationCountsDatum } from 'src/io/getMutationCounts'
+import { AminoacidMutationBadge } from 'src/components/Common/MutationBadge'
+import { TableSlim } from 'src/components/Common/TableSlim'
+import { Link } from 'src/components/Link/Link'
+import styled from 'styled-components'
+
+const NUM_ROWS_IN_MUTATION_COUNTS_SUMMARY = 20
+
+export const Table = styled(TableSlim)`
+  max-width: 500px;
+`
+
+export function formatError(error: unknown) {
+  let message = 'Unable to load data: '
+  if (error instanceof Error) {
+    message += error.message
+  }
+  return message
+}
 
 export interface MutationCountsSummaryRowProps {
   total: number
@@ -16,7 +34,9 @@ export function MutationCountsSummaryRow({ total, counts }: MutationCountsSummar
   const freq = useMemo(() => ((100 * counts.count) / total).toFixed(2), [counts.count, total])
   return (
     <tr>
-      <td>{counts.mut}</td>
+      <td>
+        <AminoacidMutationBadge mutation={counts.mut} />
+      </td>
       <td>{counts.count}</td>
       <td>{freq}</td>
     </tr>
@@ -28,6 +48,12 @@ export function useMutationCounts(clusterBuildName: string) {
     ['mutationCounts', clusterBuildName],
     async () => {
       const mutationCounts = await getMutationCounts(clusterBuildName)
+      const [geneS, geneNonS] = partition(mutationCounts.counts, (x) => x.mut.gene === 'S') // eslint-disable-line lodash/matches-prop-shorthand
+      const counts = [
+        ...geneS.slice(0, NUM_ROWS_IN_MUTATION_COUNTS_SUMMARY / 2),
+        ...geneNonS.slice(0, NUM_ROWS_IN_MUTATION_COUNTS_SUMMARY / 2),
+      ]
+      return { ...mutationCounts, counts }
     },
     {
       staleTime: Number.POSITIVE_INFINITY,
@@ -44,25 +70,44 @@ export interface MutationCountsSummaryProps {
 }
 
 export function MutationCountsSummary({ currentCluster }: MutationCountsSummaryProps) {
-  const { data, isError, isLoading } = useMutationCounts('21A.Delta')
+  const { data, isError, error, isLoading } = useMutationCounts(currentCluster.build_name)
 
   return (
-    <div>
-      <TableSlim>
-        <thead>
-          <tr>
-            <th>{'Mutation'}</th>
-            <th>{'Count'}</th>
-            <th>{'Frequency'}</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data &&
-            data.counts.map((counts) => (
-              <MutationCountsSummaryRow key={counts.mut} counts={counts} total={data.total} />
-            ))}
-        </tbody>
-      </TableSlim>
-    </div>
+    <Row noGutters>
+      <Col>
+        <div>
+          <h5 className="d-inline">{'Mutation counts summary '}</h5>
+          <span>{'('}</span>
+          <span>
+            <Link href="/">{'Details'}</Link>
+          </span>
+          <span>{')'}</span>
+        </div>
+
+        {data && (
+          <Table>
+            <thead>
+              <tr>
+                <th>{'Mutation'}</th>
+                <th>{'Count'}</th>
+                <th>{'Frequency'}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.counts.map((counts) => (
+                <MutationCountsSummaryRow key={counts.key} counts={counts} total={data.total} />
+              ))}
+            </tbody>
+          </Table>
+        )}
+        {isLoading && <div>{'Loading...'}</div>}
+        {isError && (
+          <div>
+            <div>{`Mutation counts are not yet available`}</div>
+            <div className="text-danger">{process.env.NODE_ENV === 'development' && formatError(error)}</div>
+          </div>
+        )}
+      </Col>
+    </Row>
   )
 }
