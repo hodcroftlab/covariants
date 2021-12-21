d610e598099b3f289f4b3ae17765d15c640ff29e
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-12-21 23:39:28 +0100

    chore: download owid case counts data

---
+++ scripts/get_owid_data.py
@@ -0,0 +1,47 @@
+import os
+from datetime import datetime, timedelta
+from urllib.request import urlretrieve
+
+import pandas as pd
+
+# Directory where this script resides
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+
+OWID_CSV_FILENAME = "owid-covid-data.csv"
+OWID_CSV_INPUT_URL = f"https://covid.ourworldindata.org/data/{OWID_CSV_FILENAME}"
+OWID_CSV_OUTPUT_PATH = os.path.join(THIS_DIR, "..", "data", "owid", OWID_CSV_FILENAME)
+
+
+def download_owid_data():
+    os.makedirs(os.path.dirname(OWID_CSV_OUTPUT_PATH), exist_ok=True)
+    urlretrieve(OWID_CSV_INPUT_URL, OWID_CSV_OUTPUT_PATH)
+    return OWID_CSV_OUTPUT_PATH
+
+
+def get_owid_dataframe():
+    return pd.read_csv(OWID_CSV_OUTPUT_PATH)
+
+
+def get_yesterday():
+    yesterday = datetime.now() - timedelta(days=1)
+    return yesterday.strftime('%Y-%m-%d')
+
+
+if __name__ == '__main__':
+    download_owid_data()
+    df = pd.read_csv(OWID_CSV_OUTPUT_PATH)
+
+    # Description of columns:
+    # https://github.com/owid/covid-19-data/blob/master/public/data/README.md
+    all_data = df[["iso_code", "date", "location", "total_cases_per_million"]]
+
+    # Drop rows starting with "OWID_". They are not for countries.
+    where_iso_code_is_not_a_country = df['iso_code'].str.startswith("OWID_")
+    all_data = all_data[~where_iso_code_is_not_a_country]
+
+    # Get data for yesterday, for example
+    where_date_is_yesterday = all_data["date"] == get_yesterday()
+    yesterday_data = all_data.loc[where_date_is_yesterday]
+
+    with pd.option_context('display.max_rows', None):
+        print(yesterday_data)
