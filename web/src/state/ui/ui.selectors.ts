0c3fda8d0f8dd924b64e25be6787a3cc91b4af25
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-02-21 07:56:16 +0100

    refactor(web): remove redux, add recoil
    This swaps state management framework from [`redux`](https://redux.js.org/) to [`recoil`](https://recoiljs.org/).
    
    Recoil is more modern, ergonomic, plays nicely with React hooks, and, hopefully with async rendering.
    
    I am planing to introduce some serious modifications to the app state shortly, so it is better to make the swap before that, to avoid basically writing the thing twice.
    
    So far we used redux for just a couple of things:
    - the state of the "Tooltip sort by" dropdown and checkbox
    - getting current page pathname in the navbar component (via connected-next-router)
    
    Both are kind of redundant also. I made navbar to use the pathname from the Next.js router, and moved dropdown state into a recoil atom.
    
    In this PR, there should be no visual or behavioral changes in the application. If there is, it's a bug.

--- web/src/state/ui/ui.selectors.ts
+++
@@ -1,4 +0,0 @@
-import type { State } from 'src/state/reducer'
-
-export const selectPerCountryTooltipSortBy = (state: State) => state.ui.perCountryTooltipSortBy
-export const selectPerCountryTooltipSortReversed = (state: State) => state.ui.perCountryTooltipSortReversed
