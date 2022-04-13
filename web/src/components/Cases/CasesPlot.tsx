69bc90a8fd668a7c2bdd4440e07a2f2732a67e49
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-04-13 07:35:14 +0200

    feat: add y axis label for cases plots

--- web/src/components/Cases/CasesPlot.tsx
+++ web/src/components/Cases/CasesPlot.tsx
@@ -1,19 +1,19 @@
 /* eslint-disable camelcase */
-import React, { useRef } from 'react'
+import React, { CSSProperties, useMemo, useRef } from 'react'
 
 import dynamic from 'next/dynamic'
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Label } from 'recharts'
 import { DateTime } from 'luxon'
 import ReactResizeDetector from 'react-resize-detector'
 
 import type { PerCountryCasesDistributionDatum } from 'src/io/getPerCountryCasesData'
-import { theme } from 'src/theme'
 import { ticks, timeDomain } from 'src/io/getParams'
 import { getClusterColor } from 'src/io/getClusters'
 import { formatDateHumanely } from 'src/helpers/format'
 import { adjustTicks } from 'src/helpers/adjustTicks'
 import { PlotPlaceholder } from 'src/components/Common/PlotPlaceholder'
 import { ChartContainerOuter, ChartContainerInner } from 'src/components/Common/PlotLayout'
+import { useTheme } from 'styled-components'
 import { CasesPlotTooltip } from './CasesPlotTooltip'
 
 export interface CasesPlotProps {
@@ -22,12 +22,22 @@ export interface CasesPlotProps {
 }
 
 export function CasesPlotComponent({ cluster_names, distribution }: CasesPlotProps) {
+  const theme = useTheme()
   const chartRef = useRef(null)
 
   const data = distribution.map(({ week, stand_total_cases, stand_estimated_cases }) => {
     const weekSec = DateTime.fromFormat(week, 'yyyy-MM-dd').toSeconds()
     return { week: weekSec, ...stand_estimated_cases, total: stand_total_cases }
   })
+
+  const yAxisLabelStyle: CSSProperties = useMemo(
+    () => ({
+      textAnchor: 'middle',
+      fontSize: '80%',
+      fill: theme.gray800,
+    }),
+    [theme],
+  )
 
   return (
     <ChartContainerOuter>
@@ -37,7 +47,7 @@ export function CasesPlotComponent({ cluster_names, distribution }: CasesPlotPro
             const adjustedTicks = adjustTicks(ticks, width ?? 0, theme.plot.tickWidthMin).slice(1) // slice ensures first tick is not outside domain
             return (
               <ResponsiveContainer aspect={theme.plot.aspectRatio} debounce={0}>
-                <AreaChart data={data} ref={chartRef}>
+                <AreaChart margin={{ left: 10, top: 12, bottom: 6, right: 12 }} data={data} ref={chartRef}>
                   <XAxis
                     dataKey="week"
                     type="number"
@@ -50,10 +60,19 @@ export function CasesPlotComponent({ cluster_names, distribution }: CasesPlotPro
                   />
                   <YAxis
                     type="number"
+                    name="Cases per million"
                     tick={theme.plot.tickStyle}
                     tickMargin={theme.plot.tickMargin?.y}
                     allowDataOverflow
-                  />
+                  >
+                    <Label
+                      style={yAxisLabelStyle}
+                      position="insideLeft"
+                      offset={0}
+                      angle={270}
+                      value={'Cases per million people'}
+                    />
+                  </YAxis>
 
                   {cluster_names.map((cluster) => (
                     <Area
