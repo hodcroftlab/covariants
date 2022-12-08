4f224e291e7b01e8fa8e5e664b05ef0e328ca57a
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-12-08 15:49:26 +0100

    refactor: add suspense to every page

--- web/src/pages/per-country.tsx
+++ web/src/pages/per-country.tsx
@@ -1,1 +1,11 @@
-export { CountryDistributionPage as default } from 'src/components/CountryDistribution/CountryDistributionPage'
+import React, { Suspense } from 'react'
+import { CountryDistributionPage as Page } from 'src/components/CountryDistribution/CountryDistributionPage'
+import { LOADING } from 'src/components/Loading/Loading'
+
+export default function CountryDistributionPage() {
+  return (
+    <Suspense fallback={LOADING}>
+      <Page />
+    </Suspense>
+  )
+}
--- web/src/pages/per-variant.tsx
+++ web/src/pages/per-variant.tsx
@@ -1,1 +1,11 @@
-export { ClusterDistributionPage as default } from 'src/components/ClusterDistribution/ClusterDistributionPage'
+import React, { Suspense } from 'react'
+import { ClusterDistributionPage as Page } from 'src/components/ClusterDistribution/ClusterDistributionPage'
+import { LOADING } from 'src/components/Loading/Loading'
+
+export default function ClusterDistributionPage() {
+  return (
+    <Suspense fallback={LOADING}>
+      <Page />
+    </Suspense>
+  )
+}
