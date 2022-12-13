f953ce306dfbdfaecc4e1bdac539ab5d93c1b81d
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-12-13 17:10:17 +0100

    feat: setup i18n

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,28 +1,25 @@
 import 'reflect-metadata'
 import 'resize-observer-polyfill/dist/ResizeObserver.global'
-
 import 'css.escape'
-
 import dynamic from 'next/dynamic'
 import React, { Suspense, useMemo } from 'react'
 import { QueryClient, QueryClientConfig, QueryClientProvider } from '@tanstack/react-query'
 import { RecoilRoot } from 'recoil'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { MDXProvider } from '@mdx-js/react'
-
+import i18n from 'src/i18n/i18n'
+import { I18nextProvider } from 'react-i18next'
 import { theme } from 'src/theme'
+import { loadPolyfills } from 'src/polyfills'
 import { DOMAIN_STRIPPED } from 'src/constants'
 import { Plausible } from 'src/components/Common/Plausible'
 import { LOADING } from 'src/components/Loading/Loading'
 import { getMdxComponents } from 'src/components/Common/MdxComponents'
 import { ErrorBoundary } from 'src/components/Error/ErrorBoundary'
-import { loadPolyfills } from 'src/polyfills'
-
-import 'src/styles/global.scss'
 import { Layout } from 'src/components/Layout/Layout'
+import 'src/styles/global.scss'
 
 const REACT_QUERY_OPTIONS: QueryClientConfig = {
   defaultOptions: {
@@ -47,6 +44,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <RecoilRoot>
           <ThemeProvider theme={theme}>
             <MDXProvider components={getMdxComponents}>
+              <I18nextProvider i18n={i18n} />
               <Plausible domain={DOMAIN_STRIPPED} />
               <QueryClientProvider client={queryClient}>
                 <Layout>
