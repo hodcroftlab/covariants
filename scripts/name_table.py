d92b79da560cb916aaf3be727aec1e31af766106
Emma Hodcroft <emmahodcroft@gmail.com>
2023-04-18 22:40:06 +0200

    add 23B to name tables

--- scripts/name_table.py
+++ scripts/name_table.py
@@ -178,6 +178,14 @@ name_table = [
         "others": [],
     },
     {
+        "clade": "23B (Omicron)",
+        "who": "Omicron",
+        "lineages": [
+            {"name": "XBB.1.16", "url": "https://cov-lineages.org/lineages/lineage_XBB.1.16.html"}
+        ],
+        "others": [],
+    },
+    {
         "clade": "20E (EU1)",
         "who": None,
         "lineages": [
