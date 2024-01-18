4813826a1fef4af5252728664338898690bff86b
ivan-aksamentov <ivan.aksamentov@gmail.com>
2024-01-18 09:09:45 +0100

    fix: make sure "variant" url param correctly filters selected variants

--- web/src/state/ClustersForPerCountryData.ts
+++ web/src/state/ClustersForPerCountryData.ts
@@ -1,4 +1,8 @@
 import copy from 'fast-copy'
+import { get } from 'lodash'
+import Router from 'next/router'
+import { convertToArrayMaybe, includesCaseInsensitive } from 'src/helpers/array'
+import { parseUrl } from 'src/helpers/parseUrl'
 import { updateUrlOnClustersSet } from 'src/state/Clusters'
 import type { Cluster } from 'src/state/Clusters'
 import { fetchPerCountryDataRaw } from 'src/io/getPerCountryData'
@@ -13,7 +17,18 @@ export const clustersForPerCountryDataAtom = atomFamilyAsync<Cluster[], string>(
       throw new Error(`Per-country data not found for region: '${region}'`)
     }
     const clusterNames = copy(perCountryData.cluster_names).sort()
-    return clusterNames.map((cluster) => ({ cluster, enabled: true }))
+    const clusters = clusterNames.map((cluster) => ({ cluster, enabled: true }))
+
+    const { query } = parseUrl(Router.asPath)
+    const enabledClusters = convertToArrayMaybe(get(query, 'variant'))
+    if (enabledClusters) {
+      return clusters.map((cluster) => ({
+        ...cluster,
+        enabled: includesCaseInsensitive(enabledClusters, cluster.cluster),
+      }))
+    }
+
+    return clusters
   },
   effects: [updateUrlOnClustersSet],
 })
