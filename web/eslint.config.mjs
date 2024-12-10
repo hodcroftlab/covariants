90373db9b171cdf95dd59de5dd72be9c7d293e87
Simon Füting <simon.fueting@tngtech.com>
2024-12-10 14:22:11 +0100

    feat-404: prune watch scripts

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -39,6 +39,7 @@ export default [
       '**/.coverage',
       '**/.playwright-report',
       '**/.cache',
+      '**/.json-autotranslate-cache',
       '**/.env',
       '**/.github',
       '**/.idea',
