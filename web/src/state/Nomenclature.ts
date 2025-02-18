6c6a0631ad683dfdd97949a42b7240130a2133e4
Simon Füting <simon.fueting@tngtech.com>
2025-02-18 15:11:06 +0100

    feat-503: persist nomenclature state across reloads

--- web/src/state/Nomenclature.ts
+++ web/src/state/Nomenclature.ts
@@ -1,6 +1,8 @@
 import { atom } from 'recoil'
+import { persistAtom } from 'src/state/persist/localStorage'
 
 export const enablePangolinAtom = atom({
   key: 'enablePangolin',
   default: false,
+  effects: [persistAtom],
 })
