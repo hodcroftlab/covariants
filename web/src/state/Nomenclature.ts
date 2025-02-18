360940783f569ef918ec65527a4198ca531eb7bd
Simon Füting <simon.fueting@tngtech.com>
2025-02-18 16:36:26 +0100

    feat-503: make card headers dynamically react to nomenclature switch

--- web/src/state/Nomenclature.ts
+++ web/src/state/Nomenclature.ts
@@ -1,7 +1,7 @@
 import { atom } from 'recoil'
 import { persistAtom } from 'src/state/persist/localStorage'
 
-export const enablePangolinAtom = atom({
+export const enablePangolinAtom = atom<boolean>({
   key: 'enablePangolin',
   default: false,
   effects: [persistAtom],
