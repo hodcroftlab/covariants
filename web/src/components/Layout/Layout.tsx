dae48905790190241ad103f540036917835e9303
Simon Füting <simon.fueting@tngtech.com>
2025-02-24 21:38:25 +0100

    feat-506: improve navbar layout when resizing the screen

--- web/src/components/Layout/Layout.tsx
+++ web/src/components/Layout/Layout.tsx
@@ -1,24 +1,11 @@
-import React, { PropsWithChildren, Suspense } from 'react'
+import React, { PropsWithChildren } from 'react'
 import { styled } from 'styled-components'
 import { Container as ContainerBase, Row, Col } from 'reactstrap'
-import Image from 'next/image'
-import { ErrorBoundary } from 'react-error-boundary'
-import { useRecoilState } from 'recoil'
-import { LastUpdated } from '../Common/LastUpdated'
 import { NavigationBar } from './NavigationBar'
 import { FooterContent } from './Footer'
-import GisaidLogoPNG from 'src/assets/images/GISAID_logo.png'
 import { ChristmasLightRope, Santa, Snowfall } from 'src/components/Common/Christmas'
-import { ChangelogButton } from 'src/components/Common/ChangelogButton'
-import { useTranslationSafe } from 'src/helpers/useTranslationSafe'
-import { LinkExternal } from 'src/components/Link/LinkExternal'
-import { FetchError } from 'src/components/Error/FetchError'
-import { ToggleTwoLabels } from 'src/components/Common/ToggleTwoLabels'
-import { enablePangolinAtom } from 'src/state/Nomenclature'
 
 export function Layout({ children }: PropsWithChildren<LayoutProps>) {
-  const { t } = useTranslationSafe()
-
   return (
     <Container fluid>
       <HeaderRow className={'gx-0'}>
@@ -28,29 +15,8 @@ export function Layout({ children }: PropsWithChildren<LayoutProps>) {
         </HeaderCol>
       </HeaderRow>
 
-      <Row className="ms-3 mt-2 d-none d-md-block gx-0">
-        <Col className="d-flex">
-          <GisaidText className="me-auto d-flex align-items-center gap-1">
-            {t('Enabled by data from {{ gisaid }}', { gisaid: '' })}
-            <LinkExternal href="https://www.gisaid.org/" icon={null}>
-              <Image src={GisaidLogoPNG} alt="GISAID" height={27} width={73} />
-            </LinkExternal>
-          </GisaidText>
-
-          <NomenclatureSwitch />
-
-          <ChangelogButton>
-            <ErrorBoundary FallbackComponent={FetchError}>
-              <Suspense>
-                <LastUpdated />
-              </Suspense>
-            </ErrorBoundary>
-          </ChangelogButton>
-        </Col>
-      </Row>
-
       <MainContainer fluid>
-        <MainRow className={'gx-0 mt-2'}>
+        <MainRow className={'gx-0 mt-4 mt-md-2'}>
           <MainCol>{children}</MainCol>
         </MainRow>
       </MainContainer>
@@ -64,22 +30,6 @@ export function Layout({ children }: PropsWithChildren<LayoutProps>) {
       <Snowfall />
       <Santa />
     </Container>
-  )
-}
-
-function NomenclatureSwitch({ className }: { className?: string }) {
-  const [enablePangolin, setEnablePangolin] = useRecoilState(enablePangolinAtom)
-
-  return (
-    <NomenclatureToggle
-      className={className}
-      identifier="nomenclature-switch"
-      title="Switch nomenclature"
-      checked={enablePangolin}
-      onCheckedChanged={setEnablePangolin}
-      labelLeft="Pangolin"
-      labelRight="Nextstrain"
-    />
   )
 }
 
@@ -127,14 +77,6 @@ const FooterCol = styled(Col)`
   padding: 0;
 `
 
-const GisaidText = styled.small`
-  font-size: 0.9rem;
-`
-
 export interface LayoutProps {
   wide?: boolean
 }
-
-const NomenclatureToggle = styled(ToggleTwoLabels)`
-  transform: scale(0.9);
-`
