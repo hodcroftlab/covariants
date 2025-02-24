925fafbb7c53a67be964e89354313f8c0e1d87f3
Simon Füting <simon.fueting@tngtech.com>
2025-02-24 14:19:34 +0100

    feat-506: improve toggle layout

--- web/src/components/Layout/Layout.tsx
+++ web/src/components/Layout/Layout.tsx
@@ -16,6 +16,73 @@ import { FetchError } from 'src/components/Error/FetchError'
 import { ToggleTwoLabels } from 'src/components/Common/ToggleTwoLabels'
 import { enablePangolinAtom } from 'src/state/Nomenclature'
 
+export function Layout({ children }: PropsWithChildren<LayoutProps>) {
+  const { t } = useTranslationSafe()
+
+  return (
+    <Container fluid>
+      <HeaderRow className={'gx-0'}>
+        <HeaderCol>
+          <NavigationBar />
+          <ChristmasLightRope />
+        </HeaderCol>
+      </HeaderRow>
+
+      <Row className="ms-3 mt-2 d-none d-md-block gx-0">
+        <Col className="d-flex">
+          <GisaidText className="me-auto d-flex align-items-center gap-1">
+            {t('Enabled by data from {{ gisaid }}', { gisaid: '' })}
+            <LinkExternal href="https://www.gisaid.org/" icon={null}>
+              <Image src={GisaidLogoPNG} alt="GISAID" height={27} width={73} />
+            </LinkExternal>
+          </GisaidText>
+
+          <NomenclatureSwitch />
+
+          <ChangelogButton>
+            <ErrorBoundary FallbackComponent={FetchError}>
+              <Suspense>
+                <LastUpdated />
+              </Suspense>
+            </ErrorBoundary>
+          </ChangelogButton>
+        </Col>
+      </Row>
+
+      <MainContainer fluid>
+        <MainRow className={'gx-0 mt-2'}>
+          <MainCol>{children}</MainCol>
+        </MainRow>
+      </MainContainer>
+
+      <FooterRow className={'gx-0'}>
+        <FooterCol>
+          <FooterContent />
+        </FooterCol>
+      </FooterRow>
+
+      <Snowfall />
+      <Santa />
+    </Container>
+  )
+}
+
+function NomenclatureSwitch({ className }: { className?: string }) {
+  const [enablePangolin, setEnablePangolin] = useRecoilState(enablePangolinAtom)
+
+  return (
+    <NomenclatureToggle
+      className={className}
+      identifier="nomenclature-switch"
+      title="Switch nomenclature"
+      checked={enablePangolin}
+      onCheckedChanged={setEnablePangolin}
+      labelLeft="Pangolin"
+      labelRight="Nextstrain"
+    />
+  )
+}
+
 const Container = styled(ContainerBase)`
   min-height: 100%;
   width: 100%;
@@ -68,70 +135,6 @@ export interface LayoutProps {
   wide?: boolean
 }
 
-function NomenclatureSwitch() {
-  const [enablePangolin, setEnablePangolin] = useRecoilState(enablePangolinAtom)
-
-  return (
-    <ToggleTwoLabels
-      identifier="nomenclature-switch"
-      title="Switch nomenclature"
-      checked={enablePangolin}
-      onCheckedChanged={setEnablePangolin}
-      labelLeft="Pangolin"
-      labelRight="Nextstrain"
-    />
-  )
-}
-
-export function Layout({ children }: PropsWithChildren<LayoutProps>) {
-  const { t } = useTranslationSafe()
-
-  return (
-    <Container fluid>
-      <HeaderRow className={'gx-0'}>
-        <HeaderCol>
-          <NavigationBar />
-          <ChristmasLightRope />
-        </HeaderCol>
-      </HeaderRow>
-
-      <Row className="ms-3 mt-2 d-none d-md-block gx-0">
-        <Col className="d-flex">
-          <GisaidText className="d-flex me-auto">
-            <span className="me-1 align-self-center">{t('Enabled by data from {{ gisaid }}', { gisaid: '' })}</span>
-            <LinkExternal className="align-self-center" href="https://www.gisaid.org/" icon={null}>
-              <Image src={GisaidLogoPNG} alt="GISAID" height={27} width={73} />
-            </LinkExternal>
-          </GisaidText>
-
-          <div className="d-flex">
-            <NomenclatureSwitch />
-
-            <ChangelogButton className="d-flex ms-auto">
-              <ErrorBoundary FallbackComponent={FetchError}>
-                <Suspense>
-                  <LastUpdated className="d-flex ms-auto" />
-                </Suspense>
-              </ErrorBoundary>
-            </ChangelogButton>
-          </div>
-        </Col>
-      </Row>
-
-      <MainContainer fluid>
-        <MainRow className={'gx-0'}>
-          <MainCol>{children}</MainCol>
-        </MainRow>
-      </MainContainer>
-
-      <FooterRow className={'gx-0'}>
-        <FooterCol>
-          <FooterContent />
-        </FooterCol>
-      </FooterRow>
-
-      <Snowfall />
-      <Santa />
-    </Container>
-  )
-}
+const NomenclatureToggle = styled(ToggleTwoLabels)`
+  transform: scale(0.9);
+`
