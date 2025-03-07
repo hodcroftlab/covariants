30638c389531413c9e12684b22098a9a6a5971bd
Simon Füting <simon.fueting@tngtech.com>
2025-03-07 14:05:57 +0100

    feat-508: update url behavior to accommodate 21I and 21J (virtual pango lineages including a "/" )

--- web/src/helpers/takeFirstMaybe.ts
+++ web/src/helpers/takeFirstMaybe.ts
@@ -14,3 +14,15 @@ export function takeFirstMaybe<T>(maybeArray: T | T[]): T | undefined {
 
   return undefined
 }
+
+export function joinAllMaybe(maybeArray: string | string[] | undefined): string | undefined {
+  if (!Array.isArray(maybeArray)) {
+    return maybeArray
+  }
+
+  if (maybeArray.length > 0) {
+    return maybeArray.join('/')
+  }
+
+  return undefined
+}
