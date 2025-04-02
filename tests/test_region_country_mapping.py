df04c666ae7b40c56c75b2a783ad14a461e69829
Simon Füting <simon.fueting@tngtech.com>
2025-04-02 14:59:35 +0200

    feat-573: extract main function

--- tests/test_region_country_mapping.py
+++ tests/test_region_country_mapping.py
@@ -7,16 +7,16 @@ from scripts.gen_region_country_mapping import reformat_region_country_mapping, 
 
 
 def test_extract_region_country():
-    region_country = extract_region_country_mapping('data/metadata.csv')
+    region_country = extract_region_country_mapping('tests/data/metadata.csv')
     expected_region_country = pl.DataFrame({"region": ['Asia', 'Oceania'], "country": [['Afghanistan', 'Cambodia', 'Japan'], ['Australia']]})
 
     assert_frame_equal(region_country, expected_region_country)
 
 
 def test_reformat_region_country():
-    df = pl.read_json('data/raw_region_country.json')
+    df = pl.read_json('tests/data/raw_region_country.json')
     region_country = reformat_region_country_mapping(df)
-    with open('data/region_country.json') as file:
+    with open('tests/data/region_country.json') as file:
         expected_region_country = json.load(file)
 
     assert expected_region_country == region_country
