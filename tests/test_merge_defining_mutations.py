15a84261904156e40274985fa9266e4063ef2977
Simon Füting <127181557+AdvancedCodingMonkey@users.noreply.github.com>
2025-05-12 11:24:44 +0200

    fix-578: give nuc-to-aa matching a fault tolerance of 1aa. (#599)
    Co-authored-by: Simon Füting <simon.fueting@tngtech.com>

--- tests/test_merge_defining_mutations.py
+++ tests/test_merge_defining_mutations.py
@@ -3,6 +3,7 @@ import os
 
 import numpy as np
 import pytest
+import polars as pl
 
 from scripts.defining_mutations.merge_defining_mutations import process_auto_generated_data, process_hand_curated_file, \
     match_nuc_to_aas, main, replace_list_of_empty_string, load_auto_generated_data, Mutation, \
@@ -16,6 +17,8 @@ AUTO_GENERATED_EDGE_CASES_TEST_DIR = os.path.join(TEST_DIR, 'auto_generated_edge
 OUTPUT_TEST_DIR = os.path.join(TEST_DIR, 'output')
 EXPECTED_OUTPUT_DIR = os.path.join(TEST_DIR, 'expected_output')
 EXPECTED_OUTPUT_EDGE_CASES_DIR = os.path.join(TEST_DIR, 'expected_output_edge_cases')
+DEFINING_MUTATIONS_DATA_DIR = 'defining_mutations'
+
 
 @pytest.mark.parametrize(
     'nuc, nuc_dict',
@@ -70,12 +73,53 @@ def test_match_nuc_to_aas():
     assert match_nuc_to_aas(nuc, aas) == 'ORF1a:S135R'
 
 
-@pytest.mark.xfail(
-    reason='Nuc to aa matching algorithm needs to be checked: https://github.com/hodcroftlab/covariants/issues/578 ')
-def test_match_missed_nuc_to_aas():
-    nuc = "X21766-"
+def test_match_fuzzy_assignment_of_nuc_to_aas():
+    nuc = "A21766-"
     aas = ['S:H69-']
     assert match_nuc_to_aas(nuc, aas) == 'S:H69-'
+
+
+def test_match_nuc_to_aas_selects_best_match():
+    nuc = "G28883C"
+    aas = [
+        "N:R203K",
+        "N:G204R",
+    ]
+    assert match_nuc_to_aas(nuc, aas) == "N:R203K"
+
+
+SILENT_MUTATION_PERCENTAGE_THRESHOLD = 0.32  # estimate derived from hand-curated data
+
+def test_match_nuc_to_aas_for_hand_curated_data():
+    coding_mutations = []
+    silent_mutations = []
+    filenames = os.listdir(DEFINING_MUTATIONS_DATA_DIR)
+    for file in filenames:
+        data = (
+            pl.read_csv(os.path.join(DEFINING_MUTATIONS_DATA_DIR, file), separator='\t')
+            .filter(pl.col('aa_change').is_not_null())
+            .with_columns(pl.col('aa_change').str.strip_chars(' '), clade=pl.lit(file[:3]))
+        )
+        silent_data = pl.read_csv(os.path.join(DEFINING_MUTATIONS_DATA_DIR, file), separator='\t').filter(pl.col('aa_change').is_null())
+        silent_mutations.append(silent_data)
+        assigned = data.with_columns(assigned_aa=pl.struct('nuc_change', 'aa_change').map_elements(
+            lambda x: match_nuc_to_aas(x['nuc_change'], [x['aa_change']]), return_dtype=pl.String))
+        coding_mutations.append(assigned)
+    coding_mutations = pl.concat(coding_mutations)
+    silent_mutations = pl.concat(silent_mutations)
+    mismatches = coding_mutations.filter(pl.col('aa_change').ne(pl.col('assigned_aa')).or_(pl.col('assigned_aa').is_null()))
+    silent_percentage = len(silent_mutations) / (len(coding_mutations) + len(silent_mutations))
+    assert len(mismatches) == 0
+    assert silent_percentage < SILENT_MUTATION_PERCENTAGE_THRESHOLD
+
+
+def test_match_nuc_to_aas_for_auto_generated_data():
+    lineages, auto_generated_mutations_raw = load_auto_generated_data(
+        os.path.join(AUTO_GENERATED_TEST_DIR, 'auto_generated.json'))
+    auto_generated_mutations = process_auto_generated_data(auto_generated_mutations_raw)
+    silent_mutations = auto_generated_mutations.filter(pl.col('aa_mutation').is_null())
+    proportion_silent = len(silent_mutations) / len(auto_generated_mutations)
+    assert proportion_silent < SILENT_MUTATION_PERCENTAGE_THRESHOLD
 
 
 @pytest.mark.xfail(
@@ -117,6 +161,9 @@ def test_load_and_process_auto_generated_data():
     assert len(auto_generated_mutations) == 268
     assert auto_generated_mutations.columns == ['lineage', 'nextstrain_clade', 'nuc_mutation', 'aa_mutation',
                                                 'reference']
+    silent_mutations = auto_generated_mutations.filter(pl.col('aa_mutation').is_null())
+    proportion_silent = len(silent_mutations) / len(auto_generated_mutations)
+    assert proportion_silent < SILENT_MUTATION_PERCENTAGE_THRESHOLD
 
 
 @pytest.mark.parametrize(
@@ -143,3 +190,13 @@ def test_main(expected_output_dir, auto_generated_test_dir):
 @pytest.mark.skipif(CI, reason='full functionality test only for debugging, skip on CI')
 def test_main_full():
     main(dry_run=True)
+
+
+@pytest.mark.skipif(CI, reason='full functionality test only for debugging, skip on CI')
+def test_match_nuc_to_aas_for_auto_generated_data_full():
+    lineages, auto_generated_mutations_raw = load_auto_generated_data(
+        os.path.join('data', 'auto_generated.json'))
+    auto_generated_mutations = process_auto_generated_data(auto_generated_mutations_raw)
+    silent_mutations = auto_generated_mutations.filter(pl.col('aa_mutation').is_null())
+    proportion_silent = len(silent_mutations) / len(auto_generated_mutations)
+    assert proportion_silent < SILENT_MUTATION_PERCENTAGE_THRESHOLD
