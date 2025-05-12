61cc29af749eacc800209ed90a2358997a50f2b0
Jonas Zarzalis (TNG) <jonas.zarzalis@tngtech.com>
2025-05-12 08:01:07 +0200

    fixup! feat-522: enhance design of defining mutation page

--- web/src/components/DefiningMutations/DefiningMutationsInfo.tsx
+++ web/src/components/DefiningMutations/DefiningMutationsInfo.tsx
@@ -10,26 +10,42 @@ export function DefiningMutationsInfo({ cluster }: { cluster: DefiningMutationCl
     <div className={`d-flex flex-column gap-2`}>
       <div>
         <span>{t('Parent lineage')}</span>
-        <div>
-          {cluster.parent ? (
-            <LineageBadge href={`/defining-mutations?variant=${cluster.parent}`} name={cluster.parent} />
-          ) : (
-            'none'
-          )}
-        </div>
+        <ParentLineageBadge parentLineage={cluster.parent} />
       </div>
       <div>
         <span>{t('Child lineages')}</span>
-        <div className="d-flex flex-wrap gap-1">
-          {cluster.children?.map((child) => (
-            <LineageBadge href={`/defining-mutations?variant=${child}`} key={child} name={child} />
-          ))}
-        </div>
+        <ChildLineageBadges childLineages={cluster.children} />
       </div>
       <div>
         <span>{t('Designation date')}</span>
         <div>{cluster.designationDate}</div>
       </div>
+    </div>
+  )
+}
+
+function ParentLineageBadge({ parentLineage }: { parentLineage?: string }) {
+  if (parentLineage === undefined) {
+    return <div>none</div>
+  }
+
+  return (
+    <div>
+      <LineageBadge href={`/defining-mutations?variant=${parentLineage}`} name={parentLineage} />
+    </div>
+  )
+}
+
+function ChildLineageBadges({ childLineages }: { childLineages?: string[] }) {
+  if (childLineages === undefined) {
+    return <div>none</div>
+  }
+
+  return (
+    <div className="d-flex flex-wrap gap-1">
+      {childLineages.map((child) => (
+        <LineageBadge href={`/defining-mutations?variant=${child}`} key={child} name={child} />
+      ))}
     </div>
   )
 }
