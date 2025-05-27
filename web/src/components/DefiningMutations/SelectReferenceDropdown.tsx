78e920f6f9a65dbea84cf79e102e29b3dd9b7553
Simon Füting <simon.fueting@tngtech.com>
2025-05-27 14:25:51 +0200

    feat-618: fix reference dropdown

--- web/src/components/DefiningMutations/SelectReferenceDropdown.tsx
+++ web/src/components/DefiningMutations/SelectReferenceDropdown.tsx
@@ -1,6 +1,10 @@
 import React, { useCallback, useId, useMemo } from 'react'
 import { useTranslationSafe } from 'src/helpers/useTranslationSafe'
-import { DropdownOption, stringsToOptions, stringToOption } from 'src/components/Common/DropdownOption'
+import {
+  DropdownOption,
+  stringsToOptionsWithLabels,
+  stringToOptionWithLabel,
+} from 'src/components/Common/DropdownOption'
 import { Dropdown } from 'src/components/Common/Dropdown'
 
 const style = { maxWidth: '300px' }
@@ -17,7 +21,7 @@ export function SelectReferenceDropdown({
   const { t } = useTranslationSafe()
   const id = useId()
 
-  const options = useMemo(() => stringsToOptions(referenceSequences), [referenceSequences])
+  const options = useMemo(() => stringsToOptionsWithLabels(referenceSequences, toSentenceCase), [referenceSequences])
 
   const onChange = useCallback(
     (newValue: DropdownOption<string>) => {
@@ -33,11 +37,20 @@ export function SelectReferenceDropdown({
       </label>
       <Dropdown
         identifier={id}
+        className={'width-5xs'}
         options={options}
-        value={stringToOption(selectedSequence)}
+        value={stringToOptionWithLabel(selectedSequence, toSentenceCase)}
         onOptionChange={onChange}
         isSearchable={true}
       />
     </div>
   )
+}
+
+const toSentenceCase = (str: string) => {
+  const s = str?.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)?.join(' ')
+  if (s === undefined) {
+    return str
+  }
+  return s.slice(0, 1).toUpperCase() + s.slice(1)
 }
