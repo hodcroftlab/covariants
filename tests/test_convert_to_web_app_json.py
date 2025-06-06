4c21d24bf51e40b7f7c26c092cc8ded1868bbae5
Simon Füting <simon.fueting@tngtech.com>
2025-06-06 15:01:16 +0200

    feat-573: add tests for include_case_counts

--- tests/test_convert_to_web_app_json.py
+++ tests/test_convert_to_web_app_json.py
@@ -6,6 +6,7 @@ from tests.data.convert_to_web_app.mock_clusters import mock_clusters
 from tests.data.convert_to_web_app.mock_colors_and_countries import mock_country_styles_all
 from tests.data.convert_to_web_app.mock_name_table import mock_name_table
 from tests.data.convert_to_web_app.mock_mutation_comparison import mock_mutation_comparison
+from tests.util import compare_files_in_paths
 
 CLUSTER_TABLES_PATH = "tests/data/convert_to_web_app/cluster_tables"
 OUTPUT_PATH = "tests/data/convert_to_web_app/output/web/data"
@@ -23,23 +24,4 @@ def test_main():
     main(mock_clusters, OUTPUT_PATH, ACK_OUTPUT_PATH, CLUSTER_TABLES_PATH, REGIONS_INPUTS, mock_country_styles_all,
          mock_name_table, mock_mutation_comparison)
 
-    for path, expected_path in zip([OUTPUT_PATH, ACK_OUTPUT_PATH], [EXPECTED_OUTPUT_PATH, EXPECTED_ACK_OUTPUT_PATH]):
-        for file_path, expected_file_path in zip(scan_sub_dirs(path), scan_sub_dirs(expected_path)):
-            compare_json_files(file_path, expected_file_path)
-
-
-def scan_sub_dirs(path):
-    paths = []
-    for entry in os.scandir(path):
-        if entry.is_dir():
-            paths += scan_sub_dirs(entry.path)
-        else:
-            paths.append(entry.path)
-    return paths
-
-def compare_json_files(file_one, file_two):
-    with open(file_one) as output_file:
-        with open(file_two) as expected_output_file:
-            output = json.load(output_file)
-            expected_output = json.load(expected_output_file)
-            assert output == expected_output
+    compare_files_in_paths([OUTPUT_PATH, ACK_OUTPUT_PATH], [EXPECTED_OUTPUT_PATH, EXPECTED_ACK_OUTPUT_PATH])
