084c5e3994801a25f487b9079d37b62816e7fcf9
Simon Füting <simon.fueting@tngtech.com>
2025-06-06 15:01:16 +0200

    feat-573: add tests for include_case_counts

--- scripts/include_case_counts.py
+++ scripts/include_case_counts.py
@@ -3,11 +3,11 @@ import pandas as pd
 import json
 import datetime
 import sys
-from helpers import to2week_ordinal, to2week_ordinal_string
+from scripts.helpers import to2week_ordinal_string
 
 # key: country names in covariants
 # value: country name in owid
-alernative_country_names = {
+alternative_country_names = {
     "USA" : "United States",
     "Czech Republic"  : "Czechia",
     "Côte d'Ivoire": "Cote d'Ivoire",
@@ -36,99 +36,104 @@ OUTPUT_CSV_PATH = os.path.join(THIS_DIR, "..", "web", "public", "data", OUTPUT_C
 THRESHOLD = 0.02 #0.03 is 3%
 PERIOD_PASS = 0.4 #0.5 is 50%
 
-columns = ["continent", "location", "date", "new_cases", "new_cases_per_million"]
-owid = pd.read_csv(OWID_CSV_INPUT_PATH, usecols=columns)
+def main(owid_csv_input_path, country_csv_input_path, output_csv_path, threshold=THRESHOLD, period_pass=PERIOD_PASS):
+    columns = ["continent", "location", "date", "new_cases", "new_cases_per_million"]
+    owid = pd.read_csv(owid_csv_input_path, usecols=columns)
 
-owid["date_formatted"] = owid["date"].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d"))
-# get ordinal dates, but as a string, so can compare to dates in perCOuntryData.json
-owid["date_2weeks"] = owid["date_formatted"].apply(to2week_ordinal_string)
+    owid["date_formatted"] = owid["date"].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d"))
+    # get ordinal dates, but as a string, so can compare to dates in perCOuntryData.json
+    owid["date_2weeks"] = owid["date_formatted"].apply(to2week_ordinal_string)
 
-owid_grouped = owid.groupby(["date_2weeks", "location"])[["new_cases_per_million", "new_cases"]].sum().reset_index()
+    owid_grouped = owid.groupby(["date_2weeks", "location"])[["new_cases_per_million", "new_cases"]].sum().reset_index()
 
-with open(COUNTRY_CSV_INPUT_PATH) as f:
-    perCountryData = json.load(f)
+    with open(country_csv_input_path) as f:
+        per_country_data = json.load(f)
 
-world_data = perCountryData["regions"][0]["distributions"]
-per_country_intro_content = perCountryData["regions"][0]["per_country_intro_content"]
-max_date = perCountryData["regions"][0]["max_date"]
-min_date = perCountryData["regions"][0]["min_date"]
-cluster_names = perCountryData["regions"][0]["cluster_names"]
+    world_data = per_country_data["regions"][0]["distributions"]
+    per_country_intro_content = per_country_data["regions"][0]["per_country_intro_content"]
+    max_date = per_country_data["regions"][0]["max_date"]
+    min_date = per_country_data["regions"][0]["min_date"]
+    cluster_names = per_country_data["regions"][0]["cluster_names"]
 
-world_data_counts = []
+    world_data_counts = []
 
-for i in range(len(world_data)):
-    country = world_data[i]["country"]
-    if country not in owid_grouped["location"].values and country not in alernative_country_names:
-        print("Attention! Country not found in owid data: " + country)
-        continue
+    for i in range(len(world_data)):
+        country = world_data[i]["country"]
+        if country not in owid_grouped["location"].values and country not in alternative_country_names:
+            print("Attention! Country not found in owid data: " + country)
+            continue
 
-    country_owid = country
-    if country in alernative_country_names:
-        country_owid = alernative_country_names[country]
+        country_owid = country
+        if country in alternative_country_names:
+            country_owid = alternative_country_names[country]
 
-    world_data_counts.append({"country": country, "distribution": []})
-    for j in world_data[i]["distribution"]:
-        cluster_counts = j["cluster_counts"]
-        total_sequences = j["total_sequences"]
-        week = j["week"]
+        world_data_counts.append({"country": country, "distribution": []})
+        for j in world_data[i]["distribution"]:
+            cluster_counts = j["cluster_counts"]
+            total_sequences = j["total_sequences"]
+            week = j["week"]
 
-        percent_counts = {c : float(n) / total_sequences for c, n in cluster_counts.items()}
+            percent_counts = {c : float(n) / total_sequences for c, n in cluster_counts.items()}
 
-        stand_total_cases = owid_grouped.loc[(owid_grouped.date_2weeks == week) & (owid_grouped.location == country_owid)]["new_cases_per_million"]
-        total_cases = owid_grouped.loc[(owid_grouped.date_2weeks == week) & (owid_grouped.location == country_owid)]["new_cases"]
+            stand_total_cases = owid_grouped.loc[(owid_grouped.date_2weeks == week) & (owid_grouped.location == country_owid)]["new_cases_per_million"]
+            total_cases = owid_grouped.loc[(owid_grouped.date_2weeks == week) & (owid_grouped.location == country_owid)]["new_cases"]
 
-        if len(stand_total_cases) > 0:
-            stand_total_cases = int(stand_total_cases.iloc[0])
-        else:  # No count data
-            continue  # Skip if no count data
+            if len(stand_total_cases) > 0:
+                stand_total_cases = int(stand_total_cases.iloc[0])
+            else:  # No count data
+                continue  # Skip if no count data
 
-        if len(total_cases) > 0:
-            total_cases = int(total_cases.iloc[0])
-        else:  # No count data
-            continue  # Skip if no count data
+            if len(total_cases) > 0:
+                total_cases = int(total_cases.iloc[0])
+            else:  # No count data
+                continue  # Skip if no count data
 
-        stand_estimated_cases = {c: round(float(n) * stand_total_cases) for c, n in percent_counts.items()}
-        stand_estimated_cases["others"] = stand_total_cases - sum(stand_estimated_cases.values())
-        percent_total_cases = total_sequences / total_cases if total_cases != 0 else None
+            stand_estimated_cases = {c: round(float(n) * stand_total_cases) for c, n in percent_counts.items()}
+            stand_estimated_cases["others"] = stand_total_cases - sum(stand_estimated_cases.values())
+            percent_total_cases = total_sequences / total_cases if total_cases != 0 else None
 
-        world_data_counts[-1]["distribution"].append({"week": week, "total_sequences": total_sequences, "stand_total_cases" : stand_total_cases, "stand_estimated_cases" : stand_estimated_cases, "percent_total_cases" : percent_total_cases})
+            world_data_counts[-1]["distribution"].append({"week": week, "total_sequences": total_sequences, "stand_total_cases" : stand_total_cases, "stand_estimated_cases" : stand_estimated_cases, "percent_total_cases" : percent_total_cases})
 
 
-### Check which countries pass the threshold
-weeks = []
-countries = []
-# First collect all weeks and countries
-for i in range(len(world_data_counts)):
-    country = world_data_counts[i]["country"]
-    countries.append(country)
-    for j in world_data_counts[i]["distribution"]:
-        week = j["week"]
-        if week not in weeks and "2020" not in week:
-            weeks.append(week)
+    ### Check which countries pass the threshold
+    weeks = []
+    countries = []
+    # First collect all weeks and countries
+    for i in range(len(world_data_counts)):
+        country = world_data_counts[i]["country"]
+        countries.append(country)
+        for j in world_data_counts[i]["distribution"]:
+            week = j["week"]
+            if week not in weeks and "2020" not in week:
+                weeks.append(week)
 
-df = pd.DataFrame(columns=sorted(weeks), index=sorted(countries))
+    df = pd.DataFrame(columns=sorted(weeks), index=sorted(countries))
 
-for i in range(len(world_data_counts)):
-    country = world_data_counts[i]["country"]
-    for j in world_data_counts[i]["distribution"]:
-        week = j["week"]
-        if week in weeks:
-            percent_total_cases = j["percent_total_cases"]
-            df[week][country]= percent_total_cases
+    for i in range(len(world_data_counts)):
+        country = world_data_counts[i]["country"]
+        for j in world_data_counts[i]["distribution"]:
+            week = j["week"]
+            if week in weeks:
+                percent_total_cases = j["percent_total_cases"]
+                df[week][country]= percent_total_cases
 
-total_weeks = len(weeks)
-df_threshold = (df>=THRESHOLD).sum(axis=1)
-countries_pass = df_threshold[(df_threshold/float(total_weeks)) >= PERIOD_PASS].index
+    total_weeks = len(weeks)
+    df_threshold = (df>=threshold).sum(axis=1)
+    countries_pass = df_threshold[(df_threshold/float(total_weeks)) >= period_pass].index
 
-world_data_counts_cutoff = [x for x in world_data_counts if x["country"] in countries_pass]
+    world_data_counts_cutoff = [x for x in world_data_counts if x["country"] in countries_pass]
 
-print(f"{len(world_data_counts_cutoff)}/{len(world_data_counts)} countries have passed threshold {THRESHOLD} and period_pass {PERIOD_PASS}")
+    print(f"{len(world_data_counts_cutoff)}/{len(world_data_counts)} countries have passed threshold {THRESHOLD} and period_pass {PERIOD_PASS}")
 
-if len(world_data_counts_cutoff) == 0:
-    with open(OUTPUT_CSV_PATH, "w") as out:
-        out.write("")
-    sys.exit("**FAILED TO FIND ANY COUNTRIES THAT PASS THRESHOLD - CHECK FOR ERRORS!**")
-    
+    if len(world_data_counts_cutoff) == 0:
+        with open(output_csv_path, "w") as out:
+            out.write("")
+        sys.exit("**FAILED TO FIND ANY COUNTRIES THAT PASS THRESHOLD - CHECK FOR ERRORS!**")
 
-with open(OUTPUT_CSV_PATH, "w") as out:
-    json.dump({"regions": [{"region": "World", "distributions" : world_data_counts_cutoff, "per_country_intro_content": per_country_intro_content, "max_date": max_date, "min_date": min_date, "cluster_names": cluster_names}]}, out, indent=2, sort_keys=True)
+
+    with open(output_csv_path, "w") as out:
+        json.dump({"regions": [{"region": "World", "distributions" : world_data_counts_cutoff, "per_country_intro_content": per_country_intro_content, "max_date": max_date, "min_date": min_date, "cluster_names": cluster_names}]}, out, indent=2, sort_keys=True)
+
+
+if __name__ == '__main__':
+    main(OWID_CSV_INPUT_PATH, COUNTRY_CSV_INPUT_PATH, OUTPUT_CSV_PATH)
