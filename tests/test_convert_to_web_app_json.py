4bd12e991e731ad3e8deb48fe60e8865ade2097c
Simon Füting <simon.fueting@tngtech.com>
2025-06-10 09:00:04 +0200

    feat-573: fix pipeline

--- tests/test_convert_to_web_app_json.py
+++ tests/test_convert_to_web_app_json.py
@@ -7,7 +7,7 @@ from tests.util import compare_files_in_paths
 
 CLUSTER_TABLES_PATH = "tests/data/convert_to_web_app/cluster_tables"
 OUTPUT_PATH = "tests/data/convert_to_web_app/output/web/data"
-ACK_OUTPUT_PATH = "tests/data/convert_to_web_app/output/web/acknowledgements"
+ACK_OUTPUT_PATH = "tests/data/convert_to_web_app/expected_output/web/acknowledgements"
 REGIONS_INPUTS = {
     "World": {
         "data": "EUClusters_data.json",
@@ -15,10 +15,10 @@ REGIONS_INPUTS = {
     }, }
 
 EXPECTED_OUTPUT_PATH = 'tests/data/convert_to_web_app/expected_output/web/data'
-EXPECTED_ACK_OUTPUT_PATH = "tests/data/convert_to_web_app/output/web/acknowledgements"
+
 
 def test_main():
     main(mock_clusters, OUTPUT_PATH, ACK_OUTPUT_PATH, CLUSTER_TABLES_PATH, REGIONS_INPUTS, mock_country_styles_all,
          mock_name_table, mock_mutation_comparison)
 
-    compare_files_in_paths([OUTPUT_PATH, ACK_OUTPUT_PATH], [EXPECTED_OUTPUT_PATH, EXPECTED_ACK_OUTPUT_PATH])
+    compare_files_in_paths([OUTPUT_PATH], [EXPECTED_OUTPUT_PATH])
